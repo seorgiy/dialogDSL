a28e0ecc8b58a93e382cf9e61dff53132a2b4f7d
Seorgiy <george.sergeev2014@gmail.com>
2025-06-08 13:57:22 +0300

    context in events

--- mod/Program/dialogDSL/core/events.c
+++ mod/Program/dialogDSL/core/events.c
@@ -3,14 +3,15 @@
 // Main function to use with event from dialog files to parse everything on fly
 void DLGO_ALL()
 {
+  aref context = GetEventData();
   aref links, link;
   makearef(links, dialog.links);
-  dialog.text =  DLGO(dialog.text, "");
+  dialog.text = DLGO(dialog.text, &context);
   int linkCount = GetAttributesNum(&links);
 
   for (int i = 0; i < linkCount; i++) {
     link = GetAttributeN(&links, i);
     string linkName = GetAttributeName(link);
-    dialog.links.(linkName) = DLGO(GetAttributeValue(link), "");
+    dialog.links.(linkName) = DLGO(GetAttributeValue(link), &context);
   }
 }
